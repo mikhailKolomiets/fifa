1fa4298f52272ce296fb6550739c1056081c543a
mihail <mihail.kolomiets@greyfinch.com>
2020-08-21 02:13:30 +0300

    FIFA-1 main site structure

---
+++ fifa-pro/src/main/java/site/fifa/FIFApp.java
@@ -0,0 +1,13 @@
+package site.fifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIFApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FIFApp.class, args);
+    }
+
+}
