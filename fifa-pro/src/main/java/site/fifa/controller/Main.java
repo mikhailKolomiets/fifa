86707e663777d9753e97978867cccd6f84130e86
mihail <mihail.kolomiets@greyfinch.com>
2020-08-27 00:57:43 +0300

    FIFA-6 static main paige

--- fifa-pro/src/main/java/site/fifa/controller/Main.java
+++ fifa-pro/src/main/java/site/fifa/controller/Main.java
@@ -2,10 +2,13 @@ package site.fifa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.fifa.service.TeamService;
 
 @RestController
+@RequestMapping("main")
 public class Main {
 
     @Autowired
