1714f077799a1055219e8f8c897b13589ae53dbf
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2020-09-02 13:34:47 +0300

    FIFA-9. Create Country. Common CountryController request mapping fixed

--- fifa-pro/src/main/java/site/fifa/controller/CountryController.java
+++ fifa-pro/src/main/java/site/fifa/controller/CountryController.java
@@ -2,6 +2,7 @@ package site.fifa.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.fifa.entity.Country;
 import site.fifa.service.CountryService;
@@ -9,12 +10,13 @@ import site.fifa.service.CountryService;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/countries")
 @RequiredArgsConstructor
 public class CountryController {
 
     private final CountryService countryService;
 
-    @GetMapping(value = "/countries")
+    @GetMapping
     public List<Country> getCountries() {
         return countryService.getCountries();
     }
