10c403207300ddd1df41fdf1ed263be5731ed028
mihail <mihail.kolomiets@greyfinch.com>
2020-09-17 15:20:41 +0300

    FIFA-28 add swagger 2 api

--- fifa-pro/src/main/java/site/fifa/controller/PlayerController.java
+++ fifa-pro/src/main/java/site/fifa/controller/PlayerController.java
@@ -1,6 +1,9 @@
 package site.fifa.controller;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,8 @@ public class PlayerController {
     @Autowired
     private PlayerService playerService;
 
-    @RequestMapping("get-team/{teamId}")
+    @ApiResponses({@ApiResponse(code = 200, message = "Return all players in the team")})
+    @GetMapping("get-team/{teamId}")
     public List<Player> getByTeamId(@PathVariable Long teamId) {
         return playerService.getByTeamId(teamId);
     }
