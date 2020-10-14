6af10f4b695afbb9c5261e1b78392b9667596350
mihail <mihail.kolomiets@greyfinch.com>
2020-10-14 21:50:58 +0300

    fix the table sorting

--- fifa-pro/src/main/java/site/fifa/FIFApp.java
+++ fifa-pro/src/main/java/site/fifa/FIFApp.java
@@ -2,11 +2,12 @@ package site.fifa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class FIFApp {
 
     public static void main(String[] args) {
