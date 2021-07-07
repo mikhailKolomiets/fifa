7e7e55f1adb5802fdfcb6be0c73ac13bda69d8dc
mihail <mihail.kolomiets@greyfinch.com>
2021-07-07 12:28:04 +0300

    sms test

---
+++ fifa-pro/src/main/java/site/fifa/dto/pojo/MPdata.java
@@ -0,0 +1,32 @@
+package site.fifa.dto.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class MPdata {
+
+    public final String ID_KEY = "id";
+    public final String DATA = "data";
+
+    private Long id;
+    private String data;
+    private String elseData;
+    private String any;
+    private Double price;
+
+    public List<MPdata> getSelfList(Map<String, List<String>> request) {
+        List<MPdata> result = Collections.emptyList();
+        int listSize = request.get(ID_KEY).size();
+        for (int i = 0; i < listSize; i++) {
+//77.244.39.136
+        }
+        return result;
+    }
+
+}
